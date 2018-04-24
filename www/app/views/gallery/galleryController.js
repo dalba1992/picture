b55d163cfc571a3d4b80e1d993c1dc27d4606d93
Axel <axel@303.media>
2018-04-24 14:39:08 +0200

    Gallery page remove api key exposition

--- www/app/views/gallery/galleryController.js
+++ www/app/views/gallery/galleryController.js
@@ -9,28 +9,7 @@
         function feedbackController($http) {
 
             var vm = this;
-            // vm.catalogues = [];
-            // var i = 0;
-            // $http.get('https://my.craftar.net/api/v0/collection/?api_key=9faa3308ba51217b3a65ac019840ad9d1d48cc53').then(function (resCatalogues) {
-            //
-            //     resCatalogues.data.objects.forEach(function (catalogue) {
-            //         vm.catalogues.push({
-            //             name: catalogue.name
-            //         });
-            //         $http.get('https://my.craftar.net/api/v0/item/?api_key=9faa3308ba51217b3a65ac019840ad9d1d48cc53&collection__uuid=' + catalogue.uuid).then(function (resItems) {
-            //             vm.catalogues[i].items = resItems.data.objects;
-            //             vm.catalogues[i].items.forEach(function (item) {
-            //                 $http.get('https://my.craftar.net/api/v0/image/?api_key=9faa3308ba51217b3a65ac019840ad9d1d48cc53&item__uuid=' + item.uuid).then(function (resImages) {
-            //                     item.images = resImages.data.objects;
-            //                 });
-            //             })
-            //
-            //         });
-            //
-            //     });
-            //     console.log(vm.catalogues);
-            // });
-
+          
         }
     }
 
